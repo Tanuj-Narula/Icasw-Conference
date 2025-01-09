7c2618bd677629ba9b4668f3c2bbd9d3ba95ec34
Tanuj Narula <tanujnarula07@gmail.com>
2025-01-09 15:40:56 +0530

    fixed images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,13 @@ function Navbar() {
   return (
     <div className='w-[100vw] h-[22vh] '>
       <div className='flex py-2 justify-between items-center w-[100vw] bg-[#e3f0e0]'>
-        <img src="../src/assets/vips_logo.png" alt="" className='h-28 ml-4'/>
+        <img src="/vips_logo.png" alt="" className='h-28 ml-4'/>
         <div className='text-center text-xl'> 
             <h1 className='text-4xl'>ICASW Conference 2025</h1>
             <p>Integrating Climate Action, AI, SDGs, and Water Management</p>
             <p>19th - 20th March, 2025</p>
         </div>
-        <img src="../src/assets/iipa.png" alt="" className='h-28 mr-4'/>
+        <img src="/iipa.png" alt="" className='h-28 mr-4'/>
       </div>
       <div className='bg-[#4a9f49] p-2'>   
         <ul className='flex justify-around pages'>
