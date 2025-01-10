a4d681e55318e07fe7fe49a10519234438dab349
Aditya <aditabhi9@gmail.com>
2025-01-10 09:52:47 +0530

    commit

--- src/pages/Agenda.jsx
+++ src/pages/Agenda.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Agenda() {
   return (
     <div className='h-auto text-justify p-10 mt-16'>
-      <h1 className="inline font-bold text-xs sm:text-2xl bg-yellow-300 border-black border-2 shadow-slate-700 shadow-md p-2 text-black rounded-tl-3xl rounded-br-3xl">Objective of the Conference (ICASW 2025):</h1>
-        <p  className="mt-8 mb-6">The primary objective of the International Conference on "Integrating Climate Action, AI, SDGs, and Water Management" (ICASW) is to foster interdisciplinary dialogue and collaboration among researchers, policymakers, industry leaders, and practitioners to address the critical challenges posed by climate change and water management through innovative, technology-driven, and sustainable solutions. Specifically, the conference aims to:</p>
+      <h1 className="sm:inline hidden font-bold text-xl  bg-yellow-300 border-black border-2 shadow-slate-700 shadow-md p-2 text-black rounded-tl-3xl rounded-br-3xl">Objective of the Conference (ICASW 2025):</h1>
+      <h1 className="sm:hidden inline font-bold text-lg  bg-yellow-300 border-black border-2 shadow-slate-700 shadow-md p-2 text-black rounded-tl-3xl rounded-br-3xl">Objective of ICASW 2025:</h1>  
+        <p  className="mt-8 text-base mb-6">The primary objective of the International Conference on "Integrating Climate Action, AI, SDGs, and Water Management" (ICASW) is to foster interdisciplinary dialogue and collaboration among researchers, policymakers, industry leaders, and practitioners to address the critical challenges posed by climate change and water management through innovative, technology-driven, and sustainable solutions. Specifically, the conference aims to:</p>
         <ol className="list-decimal mt-6 list-inside w-[90%] ml-3 sm:ml10">
   <li className='sm:text-lg text-base mb-4'>
     <strong>Promote Cross-Sectoral Solutions:</strong>
