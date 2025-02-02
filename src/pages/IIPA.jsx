46cb61645e1930eb5eb8e014e46ef18073930e96
Tanuj Narula <tanujnarula07@gmail.com>
2025-02-02 22:16:58 +0530

    fixing

--- src/pages/IIPA.jsx
+++ src/pages/IIPA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function IIPA() {
   return (
     <div className='h-auto sm:text-justify px-10 mt-5'>
-        <img src='/ic-icasw2025/images/iipabg1.jpg' alt='iipa' className='h-[50vh] rounded-sm w-full mb-8 shadow-[0px_5px_12px_#000000a5]' />
+        <img src='/images/iipabg1.jpg' alt='iipa' className='h-[50vh] rounded-sm w-full mb-8 shadow-[0px_5px_12px_#000000a5]' />
         <h1 className="inline font-bold text-lg sm:text-2xl bg-yellow-300 border-black border-2 p-2 text-black rounded-[7px] shadow-heading">About IIPA</h1>
         <p  className="mt-8 "><b>Indian Institute of Public Administration (IIPA) </b>an ISO 9001:2015 Certified Institution for Quality Management System, is an autonomous academic institution of national eminence for training, research and information dissemination in streams related to the ethos of Public Administration. It is now known internationally as a premier centre for Training and Research in Public Administration and Management.IIPA has advanced in academic excellence, research contributions,institutional collaborations and policy engagement, aiming to enhance effective governance. It has intensified research output and publications in public administration and policy domains. Embracing digital transformation, IIPA integrated new technologies into teaching, administration, and research. IIPA forged partnerships with global institutions, facilitating knowledge exchange and joint research projects. Furthermore, the institute expanded its programmes to meet evolving public administration needs.</p>
     </div>
--- src/pages/Speakers.jsx
+++ src/pages/Speakers.jsx
@@ -5,56 +5,56 @@ function App() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-8">
        <SpeakerCard
-        img="/ic-icasw2025/images/david.png"
+        img="/images/david.png"
         name="David Sanderson"
         designation="Professor and Inaugural Judith Neilson Chair"
         company="in Architecture, Faculty of Arts, Design and Architecture"
         location="UNSW Sydney, Australia"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/Nibedita.png"
+        img="/images/Nibedita.png"
         name="Prof. Nibedita S. Ray-Bennett FRGS, FHEA"
         designation="Professor of Risk Management"
         company="School of Business 0.26 Teaching Centre"
         location="Brookfield University of Leicester, Leicester"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/Prateek.png"
+        img="/images/Prateek.png"
         name="Prof. Prateek Sharma"
         designation="Vice – Chancellor"
         company="Delhi Technological University"
         location=""
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/harish.png"
+        img="/images/harish.png"
         name="Prof. Harish Hirani HAG"
         designation="Former Director CSIR"
         company="(CMERI, Durgapur) Dept. of Mech. Engg."
         location="IIT Delhi, ASME Fellow"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/Prosun.png"
+        img="/images/Prosun.png"
         name="Prof. Prosun Bhattacharya"
         designation="Professor of Groundwater Chemistry"
         company="Dept. of Sustainable Development, Environmental Science and Engineering"
         location="KTH Royal Institute of Technology, Stockholm, Sweden"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/pankaj.png"
+        img="/images/pankaj.png"
         name="Dr. Pankaj Thakur"
         designation="Associate Professor"
         company="Special Centre for Nano Science"
         location="JNU, Delhi"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/Ranu.png"
+        img="/images/Ranu.png"
         name="Prof. (Dr.) Ranu Gadi"
         designation="Professor,Dean"
         company="Dept. of App. Sciences & Humanities,(Industrial Res. & Development and Res. & Consultancy)"
         location="Indira Gandhi Delhi Technical University for Women"
       />
       <SpeakerCard
-        img="/ic-icasw2025/images/sujeet.png"
+        img="/images/sujeet.png"
         name="Prof. Sujeet Kumar Sinha"
         designation="Expertise Biotribology and Viomaterial"
         company="Dept. of Mechanical Engineering IIT Delhi"
